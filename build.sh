a0ca95921be4feea7af3d4aff0689672a25156c7
Alex Rosengarten <alxrsngrtn@google.com>
2019-07-13 22:48:45 -0700

    fixed syntax highlighting

--- build.sh
+++ build.sh
@@ -6,9 +6,10 @@ echo "Rendering Site"
 find . -name "*.md" -type f -exec \
  pandoc -B includes/header.html \
         -A includes/footer.html \
-        --highlight-style=breezeDark \
-        -c css/main.css \
+        --highlight-style=kate \
+        -c /css/main.css \
         --email-obfuscation=javascript \
         -t html5 \
         -o {}.html {} \;
 find . -depth -name '*.md.html' -execdir bash -c 'mv "$1" "${1/md.html/html}"' bash {} \;
+echo "Rendering complete"
