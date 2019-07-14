b71d722ef3de5c2f1b277432bfe0becf928db5a4
Alex Rosengarten <alxrsngrtn@google.com>
2019-07-13 22:08:55 -0700

    pandoc build is getting better and better

--- build.sh
+++ build.sh
@@ -3,6 +3,12 @@
 # Inspired by Will Styler: http://wstyler.ucsd.edu/posts/lmimg/spcv.txt
 
 echo "Rendering Site"
-find src -name "*.md" -type f -exec pandoc -B includes/header.html -A includes/footer.html -o {}.html {} \;
+find . -name "*.md" -type f -exec \
+ pandoc -B includes/header.html \
+        -A includes/footer.html \
+        --highlight-style=breezeDark \
+        -c css/main.css \
+        --email-obfuscation=javascript \
+        -t html5 \
+        -o {}.html {} \;
 find . -depth -name '*.md.html' -execdir bash -c 'mv "$1" "${1/md.html/html}"' bash {} \;
-find src -depth -name '*.md.html' -execdir bash -c 'mv "$1" ..' bash {} \;
